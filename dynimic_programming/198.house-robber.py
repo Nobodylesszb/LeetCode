d9f8a0d256fb0d0922f888095b6bd252072fa0be
张博 <nobodylesszb@163.com>
2019-09-24 14:20:10 +0800

    动态规划

--- dynimic_programming/198.house-robber.py
+++ dynimic_programming/198.house-robber.py
@@ -34,3 +34,17 @@ class Solution:
                 
         return now
 
+    def rob1(self,nums):
+        if not nums:
+            return 0
+
+        length = len(nums)
+        if length ==1:
+            return nums[0]
+        else:
+            prev = nums[0]
+            cur = max(prev,nums[1])
+            for i in range(2,length):
+                cur,prev = max(prev + nums[i], cur), cur
+            return cur
+
