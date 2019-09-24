274c4236125625c2028cccb31ea683e5c0ba7355
张博 <nobodylesszb@163.com>
2019-09-24 15:02:35 +0800

    删除链表元素

--- listnode/203.remove-linked-list-elements.py
+++ listnode/203.remove-linked-list-elements.py
@@ -43,3 +43,15 @@ class Solution:
         return dummy_head.next
 
 
+        def removeElements1(self,head:ListNode,val:int)->ListNode:
+            prev = ListNode(0)
+            prev.next = head
+            cur = prev
+            while cur.next:
+                if cur.next.val == val:
+                    cur.next == cur.next.next
+                else:
+                    cur = cur.next
+            return prev.next
+
+
