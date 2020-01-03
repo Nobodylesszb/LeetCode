a4cdf10097a4d7e4c217b56bc58a54ba033b15b0
Nobodylesszb <nobodylesszb@163.com>
2020-01-03 21:38:16 +0800

    增加leetcode刷题

--- array/53.maximum-sum-subarray-cn.py
+++ array/53.maximum-sum-subarray-cn.py
@@ -7,6 +7,7 @@
 # Follow up:
 # If you have figured out the O(n) solution, 
 # try coding another solution using the divide and conquer approach, which is more subtle.
+import sys
 
 class Solution:
     def maxSubArray(self,nums):
@@ -30,3 +31,18 @@ class Solution1:
             max_sum = max(max_sum_ending_curr_index, max_sum)
 
         return max_sum
+
+
+#暴力破解
+class Solution2:
+    def maxSubArray(self,nums):
+        n = len(nums)
+        maxSum = -sys.maxsize
+        sum = 0
+        for i in range(n):
+            sum = 0
+            for j in range(i, n):
+                sum += nums[j]
+                maxSum = max(maxSum, sum)
+        
+        return maxSum
